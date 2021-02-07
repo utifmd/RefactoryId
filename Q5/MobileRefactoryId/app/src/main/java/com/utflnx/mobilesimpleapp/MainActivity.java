4a4c81ace61695d65153d6b6b86ec8d324e85d14
Utif Milkedori <utifmd@gmail.com>
2021-02-07 15:58:38 +0700

    need more time

--- Q5/MobileRefactoryId/app/src/main/java/com/utflnx/mobilesimpleapp/MainActivity.java
+++ Q5/MobileRefactoryId/app/src/main/java/com/utflnx/mobilesimpleapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.utflnx.mobilesimpleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity{
     private final String TAG = getClass().getSimpleName();
     private TodoAdapter todoAdapter;
@@ -13,5 +18,16 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        todoRecyclerView = findViewById(R.id.rv_main);
+
+        todoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        todoRecyclerView.setHasFixedSize(true);
+
+        TodoModel item1 = new TodoModel();
+        TodoModel item2 = new TodoModel();
+
+        item1.setTitle("");
+
+        todoAdapter.setData();
     }
 }
